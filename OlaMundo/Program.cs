94cfe2bd0d35026f3ca2bda9464c789af63b842b
PmaiWoW <PmaiWoW@Gmail.com>
2019-03-01 14:50:09 +0000

    Added XML Documentation example in the code of OlaMundo

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,6 +2,10 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// Program to write "Hello World!" and "Goodbye World!" on the screen;
+    /// Waits for any input to shutdown the program after showing the messages;
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
