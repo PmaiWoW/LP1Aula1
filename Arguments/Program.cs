4d1d0572995a6fe6680523bd7936ce60de309796
PmaiWoW <PmaiWoW@Gmail.com>
2019-03-01 14:41:36 +0000

    Created program to write arguments given to it on the screen

---
+++ Arguments/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Argument " + (i + 1) + ": " + args[i]);
+            }
+            Console.Read();//ReadKey doesn't work in Git Bash, use Read instead
+        }
+    }
+}
