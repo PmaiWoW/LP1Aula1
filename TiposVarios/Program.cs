6058489ac41d4be0e600cc74f305181e0b7bed15
PmaiWoW <PmaiWoW@Gmail.com>
2019-03-07 23:37:12 +0000

    Alterada a solução TiposVarios, adicionando chars à mesma

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace TiposVarios
 {
@@ -6,17 +7,38 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
             //1 - Inteiros
+
             //1.1 - Hexadecimal
             int i1 = 0x15;
             uint j1 = 0x18U;
+
             //1.2 - binário
             int i2 = 0b01111001;
+
             //testar com underscore
             int i3 = 0b0111_1001;
             long l = 9L;
             ulong uL = 9UL;
-            Console.WriteLine(string.Format("{0}, {1}, {2}, {3}, {4}, {5}", i1, j1, i2, i3, l, uL));
+
+            Console.WriteLine(string.Format("Inteiros:\n" +
+                "{0},{1},{2},{3},{4},{5}", 
+                i1, j1, i2, i3, l, uL));
+
+            //2 - Chars
+            char omegaSymbol = '\u03A9';
+            char newLine = '\u000A';
+            char o = '\u004F';
+            char m = '\u004D';
+            char e = '\u0045';
+            char g = '\u0047';
+            char a = '\u0041';
+
+            Console.WriteLine("Chars:{0} {1}{0}{2}{3}{4}{5}{6}"
+                ,newLine,omegaSymbol,o,m,e,g,a);
+
             Console.ReadKey();
         }
     }
